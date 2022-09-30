d695299e820286435bf7d46c277b38b876c865ba
Paulasousa1 <silvasousapaulakarine@hotmail.com>
2022-09-30 16:36:25 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,13 @@
+var string = "WEB DEVELOPER";
+var array = string.split("");
+var timer;
+
+function frameLooper() {
+  if (array.length > 0) {
+    document.getElementById("text").innerHTML += array.shift();
+  } else {
+    clearTimeout(timer);
+  }
+  loopTimer = setTimeout("frameLooper()", 90);
+}
+frameLooper();
